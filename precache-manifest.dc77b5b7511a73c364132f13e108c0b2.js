6851b87420586b4b07f90b706de9fd3a00660ac6
Ruslan.Belous <r.bilous@viseven.com>
2022-08-31 14:16:51 +0300

    deploy

---
+++ precache-manifest.dc77b5b7511a73c364132f13e108c0b2.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b001bb86843c7c1e633",
+    "url": "/themoviedb_practices/css/app.674a852f.css"
+  },
+  {
+    "revision": "c99ec5bea8a38f66174e",
+    "url": "/themoviedb_practices/css/chunk-vendors.34378d7f.css"
+  },
+  {
+    "revision": "39bfea5e86f5f41c9d2896dbbed6791b",
+    "url": "/themoviedb_practices/fonts/MaterialIcons-Regular.39bfea5e.woff"
+  },
+  {
+    "revision": "ae8b1248595e70a828b880b9c56963da",
+    "url": "/themoviedb_practices/fonts/MaterialIcons-Regular.ae8b1248.eot"
+  },
+  {
+    "revision": "b073f5972d9c4cc1b8ae8e071e441376",
+    "url": "/themoviedb_practices/fonts/MaterialIcons-Regular.b073f597.woff2"
+  },
+  {
+    "revision": "b9968c1adf542baec8d2b298d3ff437c",
+    "url": "/themoviedb_practices/fonts/MaterialIcons-Regular.b9968c1a.ttf"
+  },
+  {
+    "revision": "eb812f85256703ec9c3d50a4ee804ca3",
+    "url": "/themoviedb_practices/img/background.eb812f85.jpeg"
+  },
+  {
+    "revision": "9c3b51140379d343d117233544bf98e5",
+    "url": "/themoviedb_practices/img/background2.9c3b5114.jpeg"
+  },
+  {
+    "revision": "9995f53719e1412cff61750eb085798b",
+    "url": "/themoviedb_practices/img/background3.9995f537.jpeg"
+  },
+  {
+    "revision": "972dfc6482f640fd8649b7c24a5a4603",
+    "url": "/themoviedb_practices/img/background4.972dfc64.jpeg"
+  },
+  {
+    "revision": "59798f3710d3a0f535cc82ec863cad2a",
+    "url": "/themoviedb_practices/img/empty.59798f37.jpeg"
+  },
+  {
+    "revision": "56fd61eec148690af48f876dfd5c8718",
+    "url": "/themoviedb_practices/img/logo.56fd61ee.svg"
+  },
+  {
+    "revision": "6160dd95d552bf0d6e9b08a9b1ccc0d1",
+    "url": "/themoviedb_practices/index.html"
+  },
+  {
+    "revision": "9b001bb86843c7c1e633",
+    "url": "/themoviedb_practices/js/app.a8462fb8.js"
+  },
+  {
+    "revision": "c99ec5bea8a38f66174e",
+    "url": "/themoviedb_practices/js/chunk-vendors.7df0953d.js"
+  },
+  {
+    "revision": "695a77817c7a04989fd5092e604fc534",
+    "url": "/themoviedb_practices/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/themoviedb_practices/robots.txt"
+  }
+]);
